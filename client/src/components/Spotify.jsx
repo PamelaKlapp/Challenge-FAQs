664cc7bf38305156e45cb4cca092884ea6ffb130
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 21:57:12 +0200

    Update Spotify
    Add variables to get spotify childrens and used map()

--- client/src/components/Spotify.jsx
+++ client/src/components/Spotify.jsx
@@ -1,8 +1,31 @@
-
+import { faqs } from '../data';
 
 const Spotify = () => {
+    const music = faqs.find((faq) => faq.name === 'Music');
+  const musicChild = music.children;
+
+  const publications = musicChild.find((faq) => faq.name === 'Publication');
+  const publiChild = publications.children
+
+  const portals = publiChild.find(
+    (faq) => faq.name === 'Portals'
+  );
+  const portalsChild = portals.children;
+
+  const spotify = portalsChild[2]
+  const spotifyChild = spotify.children
+
   return (
-    <div>Spotify</div>
+    <>
+    {spotifyChild.map(({ question, answer }, id) => {
+        return (
+          <div className="my-5" key={id}>
+            <h4 className="font-bold bg-gray-200">{question}</h4>
+            <p>{answer}</p>
+          </div>
+        );
+      })}
+    </>
   )
 }
 
