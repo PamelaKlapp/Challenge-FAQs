3d31d14ff4506b7e78820612751cd545f8d7092e
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 20:10:44 +0200

    Update Music.jsx
    Add useState to selectCategory and HandleChange to get option value.
    Used map()to get music.childrens

--- client/src/components/Music.jsx
+++ client/src/components/Music.jsx
@@ -1,7 +1,33 @@
+import { useState } from 'react'
+import {faqs} from '../data'
 
 const Music = () => {
+    const [selectedCategory, setSelectedCategory] = useState('')
+
+    const handleCategoryChange = e =>{
+        setSelectedCategory(e.target.value)
+    }
+
+    const music = faqs.find((faq)=> faq.name === 'Music')
+    const musicChild = music.children
+    console.log(musicChild)
+
   return (
-    <div>Music</div>
+    <>
+        <label className="my-5 block mb-3 font-bold text-gray-600 uppercase">Music Questions</label>
+            <select className="w-full p-3 bg-white border border-gray-400 rounded-md"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            >
+                <option value=""> -- Select Category --</option>
+                {musicChild.map((faq, i)=>{
+                    return(
+                        <option key={i}>{faq.name}</option>
+                    )
+                })}
+
+            </select>
+    </>
   )
 }
 
