8686bcc652ed603a4d43e2ca1b39ec77affd8f7e
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 19:25:20 +0200

    Add Form

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,3 +1,5 @@
+import Form from "./Form"
+
 
 const Content = () => {
   return (
@@ -5,7 +7,7 @@ const Content = () => {
         <header className="my-20">
             <h1 className="text-black text-center text-4xl font-black">FAQs</h1>
         </header>
-        <main className="bg-white md:w-2/3 lg:w-2:4 mx-auto shadow rounded-md p-10"> Formulario</main>
+        <main className="bg-white md:w-2/3 lg:w-2:4 mx-auto shadow rounded-md p-10"> <Form/></main>
         
         
     </>
