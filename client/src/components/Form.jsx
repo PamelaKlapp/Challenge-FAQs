65ea6a5311fa67c6f647252c3dd95426adcad452
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 19:29:12 +0200

    Update Form
    Add data.js and create options using data info.

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,3 +1,4 @@
+import {faqs} from '../data'
 
 const Form = () => {
   return (
@@ -6,6 +7,12 @@ const Form = () => {
             <label className="block mb-3 font-bold text-gray-600 uppercase">Category</label>
             <select name="category" className="w-full p-3 bg-white border border-gray-400 rounded-md">
                 <option value=""> -- Select Category --</option>
+                {faqs.map((faq, i)=>{
+                    return(
+                        <option key={i}>{faq.name}</option>
+                    )
+                })}
+
             </select>
         </div>
     </form>
