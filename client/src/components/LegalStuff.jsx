1876f7c0194c8f54d8205bcc8f5029655c85bc87
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 19:52:24 +0200

    Update LegalStuff.jsx
    add data.js
    create variables for childrens of legal stuff category.
    add map() to get q & a

--- client/src/components/LegalStuff.jsx
+++ client/src/components/LegalStuff.jsx
@@ -1,8 +1,24 @@
-
+import {faqs} from '../data'
 
 const LegalStuff = () => {
+    const lStuff = faqs.find((faq)=> faq.name === 'Legal stuff')
+    console.log(lStuff)
+    const lStuffChild = lStuff.children
+    console.log(lStuffChild)
+
+
   return (
-    <div>LegalStuff</div>
+    <>
+        {lStuffChild.map(({question, answer}, id) =>{
+            return(
+                <div key={id}>
+                    <h4>{question}</h4>
+                    <p>{answer}</p>
+                </div>
+            )
+        })}
+    </>
+    
   )
 }
 
