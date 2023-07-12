24bf89ab74b12259c86939ade43539cd0ba1ece2
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 19:03:49 +0200

    Create data.js

---
+++ client/src/data.js
@@ -0,0 +1,138 @@
+export const faqs = [
+    {
+      type: 'category',
+      name: 'Music',
+      children: [
+        {
+          type: 'category',
+          name: 'General questions and requirements',
+          children: [
+            {
+              type: 'faq',
+              question: 'Do I need a label?',
+              answer:
+                'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+            },
+            {
+              type: 'faq',
+              question: 'Where is my music going to be distributed?',
+              answer:
+                'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+            },
+            {
+              type: 'faq',
+              question: 'Can i name my mixes and versions by myself?',
+              answer:
+                'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+            },
+          ],
+        },
+        {
+          type: 'category',
+          name: 'Publication',
+          children: [
+            {
+              type: 'category',
+              name: 'Portals',
+              children: [
+                {
+                  type: 'faq',
+                  question: 'Can i deselect single portals?',
+                  answer:
+                    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                },
+                {
+                  type: 'faq',
+                  question:
+                    'How long does it take until my release is publicly visible?',
+                  answer:
+                    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                },
+                {
+                  type: 'category',
+                  name: 'Spotify',
+                  children: [
+                    {
+                      type: 'faq',
+                      question:
+                        'My release has been listed on the wrong artist profile.',
+                      answer:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                    },
+                    {
+                      type: 'faq',
+                      question:
+                        'Where can I see how often a specific track has been streamed?',
+                      answer:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                    },
+                  ],
+                },
+                {
+                  type: 'category',
+                  name: 'Apple Music',
+                  children: [
+                    {
+                      type: 'faq',
+                      question: "Why isn't my release listed at apple music?",
+                      answer:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                    },
+                    {
+                      type: 'faq',
+                      question: 'How can I list my release at Shazam?',
+                      answer:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'category',
+              name: 'Explanations',
+              children: [
+                {
+                  type: 'faq',
+                  question: 'What is a release date?',
+                  answer:
+                    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                },
+                {
+                  type: 'faq',
+                  question: 'What is an album, a single or a compilation?',
+                  answer:
+                    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      name: 'Legal stuff',
+      children: [
+        {
+          type: 'faq',
+          question: 'Do I have to be a GEMA member?',
+          answer:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+        },
+        {
+          type: 'faq',
+          question: 'What happens when my song will be a hit?',
+          answer:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+        },
+        {
+          type: 'faq',
+          question: 'What happens if a record label wants to have my song?',
+          answer:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+        },
+      ],
+    },
+  ];
+  
