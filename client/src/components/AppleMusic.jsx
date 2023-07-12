58a3fa86eba5e48cf3547469d7bb44b27b074b44
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 21:57:58 +0200

    Update AppleMusic.jsx
    create variables to get appleMusic childrens
    used map to get the info

--- client/src/components/AppleMusic.jsx
+++ client/src/components/AppleMusic.jsx
@@ -1,7 +1,31 @@
+import { faqs } from '../data';
 
 const AppleMusic = () => {
+    const music = faqs.find((faq) => faq.name === 'Music');
+  const musicChild = music.children;
+
+  const publications = musicChild.find((faq) => faq.name === 'Publication');
+  const publiChild = publications.children
+
+  const portals = publiChild.find(
+    (faq) => faq.name === 'Portals'
+  );
+  const portalsChild = portals.children;
+
+  const appleMusic = portalsChild[3]
+  const appleMusicChild = appleMusic.children
+
   return (
-    <div>AppleMusic</div>
+    <>
+    {appleMusicChild.map(({ question, answer }, id) => {
+        return (
+          <div className="my-5" key={id}>
+            <h4 className="font-bold bg-gray-200">{question}</h4>
+            <p>{answer}</p>
+          </div>
+        );
+      })}
+    </>
   )
 }
 
