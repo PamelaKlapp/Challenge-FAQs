6167e8b4985fa5c9ace3b8976b2545e8a4fb1aaa
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 19:40:33 +0200

    Update Fomr
    Add components Music/LegalStuff
    Create useStare for selectedCategory and HandleChange

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import {faqs} from '../data'
+import Music from './Music'
+import LegalStuff from './LegalStuff'
 
 const Form = () => {
+    const [selectedCategory, setSelectedCategory] = useState('')
+
+    const handleCategoryChange = e =>{
+        setSelectedCategory(e.target.value)
+    }
+
   return (
     <form>
         <div className="my-5">
             <label className="block mb-3 font-bold text-gray-600 uppercase">Category</label>
-            <select name="category" className="w-full p-3 bg-white border border-gray-400 rounded-md">
+            <select name="category" className="w-full p-3 bg-white border border-gray-400 rounded-md"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            >
                 <option value=""> -- Select Category --</option>
                 {faqs.map((faq, i)=>{
                     return(
@@ -14,6 +26,8 @@ const Form = () => {
                 })}
 
             </select>
+            {selectedCategory === 'Music' && <Music/>}
+            {selectedCategory === 'Legal stuff' && <LegalStuff/>}
         </div>
     </form>
   )
