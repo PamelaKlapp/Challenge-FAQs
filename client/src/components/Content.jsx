886121b4e1ecda63a7bab829255336b5bcf4bcdf
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 19:16:10 +0200

    Create Content.jsx

---
+++ client/src/components/Content.jsx
@@ -0,0 +1,15 @@
+
+const Content = () => {
+  return (
+    <>
+        <header className="my-20">
+            <h1 className="text-black text-center text-4xl font-black">FAQs</h1>
+        </header>
+        <main className="bg-white md:w-2/3 lg:w-2:4 mx-auto shadow rounded-md p-10"> Formulario</main>
+        
+        
+    </>
+  )
+}
+
+export default Content
