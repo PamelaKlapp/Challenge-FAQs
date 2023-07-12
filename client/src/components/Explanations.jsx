9020aac2014612668d8d5a6432be1864f5fdc4d0
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 20:54:11 +0200

    Update Explanations.jsx
    Add variables to get explanations childrens.
    
    add map()to get explanations childrens

--- client/src/components/Explanations.jsx
+++ client/src/components/Explanations.jsx
@@ -1,8 +1,29 @@
-
+import { faqs } from '../data';
 
 const Explanations = () => {
+  const music = faqs.find((faq) => faq.name === 'Music');
+  const musicChild = music.children;
+
+  const publications = musicChild.find((faq) => faq.name === 'Publication');
+  const publiChild = publications.children
+
+  const explanations = publiChild.find(
+    (faq) => faq.name === 'Explanations'
+  );
+  const explanationsChild = explanations.children;
+  console.log(explanationsChild)
+
   return (
-    <div>Explanations</div>
+   <>
+    {explanationsChild.map(({ question, answer }, id) => {
+        return (
+          <div className="my-5" key={id}>
+            <h4 className="font-bold bg-gray-100">{question}</h4>
+            <p>{answer}</p>
+          </div>
+        );
+      })}
+   </>
   )
 }
 
