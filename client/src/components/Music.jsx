06c446bfed2c804a53b47addd86bb6324aaa7133
PamelaKlapp <pameklapp@gmail.com>
2023-07-12 20:25:56 +0200

    Update Music.jsx
    Add components GQuestions & Publications.
    
    Deleted console.log()

--- client/src/components/Music.jsx
+++ client/src/components/Music.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import {faqs} from '../data'
+import { useState } from 'react';
+import { faqs } from '../data';
+import GQuestions from './GQuestions';
+import Publications from './Publications';
 
 const Music = () => {
-    const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState('');
 
-    const handleCategoryChange = e =>{
-        setSelectedCategory(e.target.value)
-    }
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
 
-    const music = faqs.find((faq)=> faq.name === 'Music')
-    const musicChild = music.children
-    console.log(musicChild)
+  const music = faqs.find((faq) => faq.name === 'Music');
+  const musicChild = music.children;
 
   return (
     <>
-        <label className="my-5 block mb-3 font-bold text-gray-600 uppercase">Music Questions</label>
-            <select className="w-full p-3 bg-white border border-gray-400 rounded-md"
-            value={selectedCategory}
-            onChange={handleCategoryChange}
-            >
-                <option value=""> -- Select Category --</option>
-                {musicChild.map((faq, i)=>{
-                    return(
-                        <option key={i}>{faq.name}</option>
-                    )
-                })}
-
-            </select>
+      <label className="my-5 block mb-3 font-bold text-gray-600 uppercase">
+        Music Questions
+      </label>
+      <select
+        className="w-full p-3 bg-white border border-gray-400 rounded-md"
+        value={selectedCategory}
+        onChange={handleCategoryChange}
+      >
+        <option value=""> -- Select Category --</option>
+        {musicChild.map((faq, i) => {
+          return <option key={i}>{faq.name}</option>;
+        })}
+      </select>
+      {selectedCategory === 'General questions and requirements' && (
+        <GQuestions />
+      )}
+      {selectedCategory === 'Publication' && <Publications />}
     </>
-  )
-}
+  );
+};
 
-export default Music
+export default Music;
